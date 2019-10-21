d4845b844b0b05a1183a18f8c0d2d031b34b9b56
shuloh <tokiya@gmail.com>
2019-10-21 18:24:02 +0800

    Updates

---
+++ precache-manifest.13216459afa2f8d97eb61fcf46e83a93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df59a2c8612a645fbe67445e1c3d2577",
+    "url": "/entanglement-exchange/index.html"
+  },
+  {
+    "revision": "0e28aa057bc8b89d8869",
+    "url": "/entanglement-exchange/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f7ef39caa363233ea100",
+    "url": "/entanglement-exchange/static/js/2.255f55fc.chunk.js"
+  },
+  {
+    "revision": "0e28aa057bc8b89d8869",
+    "url": "/entanglement-exchange/static/js/main.f3778b94.chunk.js"
+  },
+  {
+    "revision": "5d2bd1dec1a90a186dcf",
+    "url": "/entanglement-exchange/static/js/runtime-main.1166b21c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/entanglement-exchange/precache-manifest.78f0078e7bbe7cabb8c598a7dd6a22af.js"
+  "/entanglement-exchange/precache-manifest.13216459afa2f8d97eb61fcf46e83a93.js"
 );
 
 self.addEventListener('message', (event) => {
