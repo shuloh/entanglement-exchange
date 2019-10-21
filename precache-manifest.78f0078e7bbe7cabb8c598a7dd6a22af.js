6aa3f683eca601cbc58c87e57931e54b81fce211
shuloh <tokiya@gmail.com>
2019-10-21 09:01:33 +0800

    Updates

---
+++ precache-manifest.78f0078e7bbe7cabb8c598a7dd6a22af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75ce927ae3b51147bfff7152cd5657e",
+    "url": "/entanglement-exchange/index.html"
+  },
+  {
+    "revision": "ef20851f93fdeced9210",
+    "url": "/entanglement-exchange/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8d901628f8f8289debd9",
+    "url": "/entanglement-exchange/static/js/2.8c6b4b1b.chunk.js"
+  },
+  {
+    "revision": "ef20851f93fdeced9210",
+    "url": "/entanglement-exchange/static/js/main.9841148d.chunk.js"
+  },
+  {
+    "revision": "5126e6a22bb009c3fed2",
+    "url": "/entanglement-exchange/static/js/runtime-main.5f5fa771.js"
+  }
+]);
